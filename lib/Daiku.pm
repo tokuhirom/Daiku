3339fc9ab3033084a297abe696f112570bc98120
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-04-22 09:37:30 +0900

    initial import

---
+++ lib/Daiku.pm
@@ -0,0 +1,39 @@
+package Daiku;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Daiku -
+
+=head1 SYNOPSIS
+
+  use Daiku;
+
+=head1 DESCRIPTION
+
+Daiku is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
